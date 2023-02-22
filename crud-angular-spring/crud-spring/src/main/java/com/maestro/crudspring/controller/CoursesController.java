cb31261cee49aa4ce9096c721f3ff7480d54d62b
Carlos Maestro <chmmaestro@gmail.com>
2023-02-21 22:43:48 -0300

    Endpoint, repository, model course

---
+++ crud-angular-spring/crud-spring/src/main/java/com/maestro/crudspring/controller/CoursesController.java
@@ -0,0 +1,25 @@
+package com.maestro.crudspring.controller;
+
+import java.util.List;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.maestro.crudspring.model.Course;
+import com.maestro.crudspring.repository.CourseRepository;
+
+import lombok.AllArgsConstructor;
+
+@RestController
+@RequestMapping("/api/courses")
+@AllArgsConstructor
+public class CoursesController {
+  
+  private final CourseRepository courseRepository;
+
+  @GetMapping
+  public List<Course> list(){
+    return this.courseRepository.findAll();
+  }
+}

cc92bb69c67d4d853a45f1f574c4805bbd061645
Carlos Maestro <chmmaestro@gmail.com>
2023-02-28 20:17:53 -0300

    criando curso frontend

--- crud-angular-spring/crud-angular/src/app/courses/course-form/course-form.component.ts
+++ crud-angular-spring/crud-angular/src/app/courses/course-form/course-form.component.ts
@@ -1,5 +1,9 @@
+import { Location } from '@angular/common';
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup } from '@angular/forms';
+import { MatSnackBar } from '@angular/material/snack-bar';
+
+import { CoursesService } from './../services/courses.service';
 
 @Component({
   selector: 'app-course-form',
@@ -9,10 +13,18 @@ import { FormBuilder, FormGroup } from '@angular/forms';
 export class CourseFormComponent implements OnInit {
 
   form: FormGroup;
+  categorias: { value: any, display: string }[];
 
   constructor(
-    private formbuilder: FormBuilder
+    private formbuilder: FormBuilder,
+    private service: CoursesService,
+    private _snackBar: MatSnackBar,
+    private location: Location
   ) {
+    this.categorias = [
+      { display: 'Front-End', value: 'front-end' },
+      { display: 'Back-End', value: 'back-end' },
+    ];
     this.form = this.formbuilder.group({
       name: [null],
       category: [null],
@@ -23,10 +35,21 @@ export class CourseFormComponent implements OnInit {
   }
 
   onCancel() {
-
+    this,this.location.back();
   }
 
   onSubmit() {
+    this.service.save(this.form.value).subscribe({
+      next: (result) => this.onSuccess(),
+      error: (error) => this.onError()
+    });
+  }
 
+  onError() {
+    this._snackBar.open('Erro ao salvar curso', '', { duration: 5000 });
+  }
+  onSuccess() {
+    this.onCancel();
+    this._snackBar.open('Curso salvo com sucesso!', '', { duration: 5000 });
   }
 }

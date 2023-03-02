557573c2ea54096c24f065a3db82913b3acb7236
Carlos Maestro <chmmaestro@gmail.com>
2023-03-01 22:45:00 -0300

    get course by id

--- crud-angular-spring/crud-spring/src/main/java/com/maestro/crudspring/controller/CoursesController.java
+++ crud-angular-spring/crud-spring/src/main/java/com/maestro/crudspring/controller/CoursesController.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -27,6 +28,13 @@ public class CoursesController {
   @GetMapping
   public @ResponseBody List<Course> list() {
     return courseRepository.findAll();
+  }
+
+  @GetMapping("/{id}")
+  public ResponseEntity<Course> findById(@PathVariable Long id) {
+    return courseRepository.findById(id)
+        .map(course -> ResponseEntity.ok().body(course))
+        .orElse(ResponseEntity.notFound().build());
   }
 
   @PostMapping

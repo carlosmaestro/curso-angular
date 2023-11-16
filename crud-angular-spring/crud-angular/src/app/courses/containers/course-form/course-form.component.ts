8dad2fc09d2dfe977293005654b5f8a52618e83e
Carlos Maestro <chmmaestro@gmail.com>
2023-11-15 22:27:13 -0300

    mensagem de erro de validacao

--- crud-angular-spring/crud-angular/src/app/courses/containers/course-form/course-form.component.ts
+++ crud-angular-spring/crud-angular/src/app/courses/containers/course-form/course-form.component.ts
@@ -1,6 +1,6 @@
 import { Location } from '@angular/common';
 import { Component, OnInit } from '@angular/core';
-import { NonNullableFormBuilder } from '@angular/forms';
+import { NonNullableFormBuilder, Validators } from '@angular/forms';
 import { MatSnackBar } from '@angular/material/snack-bar';
 
 import { ActivatedRoute } from '@angular/router';
@@ -15,8 +15,11 @@ import { Course } from '../../model/course';
 export class CourseFormComponent implements OnInit {
   form = this.formbuilder.group({
     _id: [''],
-    name: [''],
-    category: [''],
+    name: [
+      '',
+      [Validators.required, Validators.minLength(5), Validators.maxLength(100)],
+    ],
+    category: ['', Validators.required],
   });
 
   categorias: { value: any; display: string }[];
@@ -49,17 +52,39 @@ export class CourseFormComponent implements OnInit {
   }
 
   onSubmit() {
+    if(this.form.invalid){
+      return;
+    }
     this.service.save(this.form.value).subscribe({
       next: (result) => this.onSuccess(),
       error: (error) => this.onError(),
     });
   }
 
-  onError() {
+  private onError() {
     this._snackBar.open('Erro ao salvar curso', '', { duration: 5000 });
   }
-  onSuccess() {
+  private onSuccess() {
     this.onCancel();
     this._snackBar.open('Curso salvo com sucesso!', '', { duration: 5000 });
+  }
+
+  getErrorMessage(fieldName: string) {
+    const field = this.form.get(fieldName);
+
+    if (field?.hasError('required')) {
+      return 'Campo obrigatório';
+    }
+    if (field?.hasError('minlength')) {
+      const requiredlength = field.errors? field.errors['minlength']['requiredLength']: 5;
+      return `Tamanho mínimo precisa ser de ${requiredlength} caracteres`;
+    }
+
+    if (field?.hasError('maxlength')) {
+      const requiredlength = field.errors? field.errors['maxlength']['requiredLength']: 200;
+      return `Tamanho máximo precisa ser de ${requiredlength} caracteres`;
+    }
+
+    return 'Campo inválido';
   }
 }
